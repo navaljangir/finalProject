9245146dd843bce59384b71f57c749c55338ba2f
Naval Kishor <jangirnaval392003@gmail.com>
2023-11-21 13:15:15 +0530

    add

---
+++ build/precache-manifest.6d40289a0b17819d7ab3ac15d414fc3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0df0abad84f0945856ab78c26ae3046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dff891c03b720654ee3",
+    "url": "/static/css/2.c99ed502.chunk.css"
+  },
+  {
+    "revision": "fddc346dd84571524d0f",
+    "url": "/static/css/main.11de75e8.chunk.css"
+  },
+  {
+    "revision": "5dff891c03b720654ee3",
+    "url": "/static/js/2.4a4ac713.chunk.js"
+  },
+  {
+    "revision": "fddc346dd84571524d0f",
+    "url": "/static/js/main.128fc274.chunk.js"
+  },
+  {
+    "revision": "a9103ff4ff773beb2898",
+    "url": "/static/js/runtime~main.3a93585e.js"
+  },
+  {
+    "revision": "42d15697c964cfabf3b2d51d5f4ddbd8",
+    "url": "/static/media/background.42d15697.jpg"
+  }
+]);
